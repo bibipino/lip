c510acdae09fc1234d4e2564f8cb15ac1cc25d38
Docker <docker@example.com>
2025-10-16 13:29:13 +0000

    Completed adder

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,10 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let addlist l = 
+  let rec adl = function
+  | [] -> 0
+  | t::h -> t + adl h
+in
+  adl l
+;; (* replace 0 with actual code *)
--- basics/adder/test/addertest.ml
+++ basics/adder/test/addertest.ml
@@ -4,3 +4,5 @@ let%test _ = addlist [] = 0
 let%test _ = addlist [3] = 3
 let%test _ = addlist [1;2] = 3
 let%test _ = addlist [1;2;3] = 6
+let%test _ = addlist [2;3;4] = 9
+let%test _ = addlist [3;4;5] = 12
